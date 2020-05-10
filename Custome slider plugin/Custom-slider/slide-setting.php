ee4ac8a0cb382fad3ce70ddceeb5995f478a6831
safik <safikskh55@gmail.com>
2020-05-10 16:05:21 +0530

    updated

--- Custome slider plugin/Custom-slider/slide-setting.php
+++ Custome slider plugin/Custom-slider/slide-setting.php
@@ -46,35 +46,40 @@ if ( isset( $_POST['submit_image_selector'] ) && isset( $_POST['image_attachment
 <div class="wrap">
 	<h1>Custom Slider Settings</h1>
 	<form method="post" action="" enctype="multipart/form-data">
-    <table class="form-table">
-        <tr valign="top">
-	        <th scope="row">Upload Image</th>
-	        <td><input type="button" name="upload-btn" id="upload-btn" class="button-secondary" value="Upload Image"></td>
-        </tr>
-        <tr valign="top">
-	        <th scope="row">Note Supported image format:</th>
-	        <td>.jpeg, .jpg, .png, .gif</td>
-        </tr>
-    </table>
-    
-    
+        <table class="form-table">
+            <tr valign="top">
+    	        <th scope="row">Upload Image</th>
+    	        <td><input type="button" name="upload-btn" id="upload-btn" class="button-secondary" value="Upload Image"></td>
+            </tr>
+            <tr valign="top">
+    	        <th scope="row">Note Supported image format:</th>
+    	        <td>.jpeg, .jpg, .png, .gif</td>
+            </tr>
+             <tr valign="top">
+                <th scope="row">Please use this shortcode for display slider:</th>
+                <td>[show_slider]</td>
+            </tr>
+        </table>
+        
+        
 
 
-    <div class="row" id="sortable">
-        <?php 
-        $fivesdrafts = $wpdb->get_results("SELECT * FROM wp_custom_slide ORDER BY order_num");
-        foreach ($fivesdrafts as $value) {
-            $img_atts = wp_get_attachment_image_src($value->img_id, 'full');
-        ?>
-        <div class="column">
-            <img class="close-btn" src="<?=plugin_dir_url( __FILE__ )?>close-icon.png" alt="Snow" style="">
-            <img src="<?=$img_atts[0]?>" alt="Snow" style="width:100%">
-            <input id="img_<?=$value->img_id?>" type="hidden" name="myplugin_attachment_id_array[]"  value="<?=$value->img_id?>">
-            <input class="order_id" name="myplugin_attachment_id_order[]" type="hidden" value="<?=$value->order_num;?>"></div>
-        <?php
-        }
-        ?>
-    </div>
+        <div class="row" id="sortable">
+            <?php 
+            $fivesdrafts = $wpdb->get_results("SELECT * FROM wp_custom_slide ORDER BY order_num");
+            foreach ($fivesdrafts as $value) {
+                $img_atts = wp_get_attachment_image_src($value->img_id, 'full');
+            ?>
+            <div class="column">
+                <img class="close-btn" src="<?=plugin_dir_url( __FILE__ )?>close-icon.png" alt="Snow" style="">
+                <img src="<?=$img_atts[0]?>" alt="Snow" style="width:100%">
+                <input id="img_<?=$value->img_id?>" type="hidden" name="myplugin_attachment_id_array[]"  value="<?=$value->img_id?>">
+                <input class="order_id" name="myplugin_attachment_id_order[]" type="hidden" value="<?=$value->order_num;?>"></div>
+            <?php
+            }
+            ?>
+        </div>
 
-<?php submit_button('Save Changes','','save_file'); ?>
-</form>
+    <?php submit_button('Save Changes','','save_file'); ?>
+    </form>
+</div>
